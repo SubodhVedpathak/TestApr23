6b1110c12e92cdfc6518f30ca8a2208844b4ec00
SubodhVedpathak <subodhsv89@gmail.com>
2023-05-23 08:57:52 +0530

    B2 changes

--- src/basicConcept/StringPrac.java
+++ src/basicConcept/StringPrac.java
@@ -3,6 +3,7 @@ package basicConcept;
 public class StringPrac {
 	
 	public static void main(String[] args) {
-		System.out.println("I am in main method");
+		System.out.println("I am in main method, B2");
+		System.out.println("B2 B2");
 	}
 }
