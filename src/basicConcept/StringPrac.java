5039be4def39d3dde580abd047fb29bf374ef03e
SubodhVedpathak <subodhsv89@gmail.com>
2023-05-23 08:50:01 +0530

    conflict eg 2 A2 commit

--- src/basicConcept/StringPrac.java
+++ src/basicConcept/StringPrac.java
@@ -3,6 +3,7 @@ package basicConcept;
 public class StringPrac {
 	
 	public static void main(String[] args) {
-		System.out.println("I am in main method");
+		System.out.println("I am in main method A2");
+		System.out.println("This is branch A2, for conflict");
 	}
 }
